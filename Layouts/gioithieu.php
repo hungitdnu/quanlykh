a697a9298b1eacb6e82aafa27067e20bc643c677
hungitdnu <hung0904202hd@gmail.com>
2024-07-10 23:46:14 +0700

    php

---
+++ Layouts/gioithieu.php
@@ -0,0 +1,62 @@
+<main>
+
+<div class="colorlib-about">
+    <div class="container">
+        <div class="row row-pb-lg">
+            <div class="col-sm-6 mb-3">
+                <div class="video colorlib-video">
+                    <img src="./images/logo3.jpg" alt="">
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="about-wrap">
+                    <h2>Khám phá sứ mệnh của chúng tôi</h2>
+                    <p>Có một thế giới nơi mà một số người đang phải đối mặt với sự khó khăn và mất phương hướng trong sự nghiệp. Chúng tôi tin rằng mỗi người đều có khả năng để tỏa sáng và phát triển.</p>
+                    <p>"Có những người ở tuổi 22 đang mơ mộng về tương lai. Có những người ở tuổi 28 đang lo lắng về sự ổn định trong cuộc sống. Và có những người ở tuổi 40 nuối tiếc về những cơ hội đã qua."</p>
+                </div>
+            </div>
+        </div>
+        <div class="dv2">
+            <p>Mọi thứ có thể thay đổi nếu họ được hướng dẫn đúng đắn trong sự nghiệp của mình, đặt ra mục tiêu rõ ràng và phát triển những kỹ năng cần thiết.</p>
+        </div>
+        <div class="dv3">
+            <h2 class="text-primary">Tầm nhìn của chúng tôi</h2>
+            <p>Chúng tôi muốn trở thành một công ty công nghệ giáo dục uy tín trên thị trường, cung cấp các sản phẩm hỗ trợ học lập trình chất lượng, thông minh và hiệu quả. Chúng tôi muốn được biết đến với sản phẩm chất lượng và sự tin tưởng từ cộng đồng, không chỉ là về khả năng tiếp thị tốt.</p>
+        </div>
+        <div class="dv4">
+            <h2 class="text-primary">Giá trị cốt lõi của chúng tôi</h2>
+            <p><b>Sự tử tế:</b> Chúng tôi đề cao sự tử tế trong tương tác với nhân viên và học viên. Sản phẩm của chúng tôi phải đạt chất lượng và hiệu quả, không phân biệt giữa sản phẩm miễn phí và sản phẩm trả phí. Chúng tôi muốn sản phẩm của mình được sử dụng bởi người thân của chính bản thân chúng tôi.</p>
+            <p><b>Tư duy số:</b> Sản phẩm của chúng tôi không chỉ đáp ứng nhu cầu của đội ngũ nội bộ. Chúng được tạo ra với mục tiêu cao nhất là giúp người học dễ dàng tiếp cận và hiểu được kiến thức, cảm hứng và khả năng tự học, và có thể áp dụng kiến thức vào thực tế.</p>
+            <p><b>Luôn đổi mới và học hỏi:</b> Chúng tôi luôn mở cửa để học hỏi từ đối thủ và các công ty giáo dục công nghệ hàng đầu trên thế giới. Chúng tôi luôn lắng nghe mọi ý kiến đóng góp từ phía học viên.</p>
+            <p><b>Tư duy bền vững:</b> Chúng tôi tin rằng đầu tư vào nhân viên và khách hàng là đầu tư có lợi nhất trong dài hạn cho một công ty.</p>
+        </div>
+        <div class="container-fuild text-center mt-5">
+            <img src="https://websitehoctructuyen.com/wp-content/uploads/2021/01/hoc-online.jpg" alt="" width="800px">
+        </div>
+        <div class="dv5 mt-3 bt-3">
+            <h2 class="text-center text-primary">Bạn sẽ nhận được gì từ chúng tôi?</h2>
+            <div class="row mx-5">
+                <div class="col-6">
+                    <h5><b>1. Sự thành thạo</b></h5>
+                    <p>Các khóa học kết hợp lý thuyết và thực hành, có bài kiểm tra ngay trên trang web và mang lại sản phẩm thực tế cho bạn sau mỗi khóa học.</p>
+                </div>
+                <div class="col-6">
+                    <h5><b>2. Tính tự học</b></h5>
+                    <p>Bạn chỉ thực sự trưởng thành trong sự nghiệp nếu biết cách tự học và cập nhật kiến thức mới cho bản thân mình.</p>
+                </div>
+            </div>
+            <div class="row mx-5">
+                <div class="col-6">
+                    <h5><b>3. Tiết kiệm thời gian</b></h5>
+                    <p>Thay vì mất vài năm, bạn chỉ cần 4-6 tháng để bắt đầu công việc đầu tiên tại các công ty IT với vị trí thực tập.</p>
+                </div>
+                <div class="col-6">
+                    <h5><b>4. Làm những điều quan trọng</b></h5>
+                    <p>Chúng tôi tập trung vào việc học và thực hiện những điều quan trọng nhất để bạn có thể đạt được mục tiêu của mình trong thời gian ngắn nhất.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+</main>
